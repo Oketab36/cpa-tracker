89a81faf1eff5c965a9429b52cd435c843d91496
Oketab36 <oketab36@gmail.com>
2026-05-11 10:09:17 +0100

    Update offerwall.js

--- pages/offerwall.js
+++ pages/offerwall.js
@@ -1,31 +1,38 @@
-export async function getServerSideProps(context) {
-  const clickId = context.query.click_id || "";
-
-  return {
-    props: {
-      clickId
-    }
-  };
-}
+import { useEffect } from "react";
 
 export default function Offerwall({ clickId }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://getafilenow.com/script_include.php?id=1895181&tracking_id=${clickId}`;
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [clickId]);
+
   return (
-    <div
-      style={{
-        backgroundColor: "#111",
-        color: "#fff",
-        minHeight: "100vh",
-        textAlign: "center",
-        padding: "30px"
-      }}
-    >
+    <div style={{
+      backgroundColor: "#111",
+      color: "#fff",
+      minHeight: "100vh",
+      textAlign: "center",
+      padding: "30px"
+    }}>
       <h1>Complete One Offer Below</h1>
       <p>Rewards unlock automatically after completion.</p>
 
-      <script
-        src={`https://getafilenow.com/script_include.php?id=1895181&tracking_id=${clickId}`}
-        async
-      ></script>
+      <div id="offerwall-container"></div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      clickId: context.query.click_id || ""
+    }
+  };
 }
