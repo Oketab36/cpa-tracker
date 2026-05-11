63e62fc65bb27c290b397f46f1e7f986297fe086
Oketab36 <oketab36@gmail.com>
2026-05-11 08:40:42 +0100

    Update offerwall.js

--- pages/offerwall.js
+++ pages/offerwall.js
@@ -1,1 +1,31 @@
+export async function getServerSideProps(context) {
+  const clickId = context.query.click_id || "";
 
+  return {
+    props: {
+      clickId
+    }
+  };
+}
+
+export default function Offerwall({ clickId }) {
+  return (
+    <div
+      style={{
+        backgroundColor: "#111",
+        color: "#fff",
+        minHeight: "100vh",
+        textAlign: "center",
+        padding: "30px"
+      }}
+    >
+      <h1>Complete One Offer Below</h1>
+      <p>Rewards unlock automatically after completion.</p>
+
+      <script
+        src={`https://getafilenow.com/script_include.php?id=1895181&tracking_id=${clickId}`}
+        async
+      ></script>
+    </div>
+  );
+}
